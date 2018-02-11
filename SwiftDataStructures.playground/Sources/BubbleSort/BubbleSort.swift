343e01b89b4252db4edb4ad73c5a7d240d767bf1
Israel P. T. da Silva <ismalakazel@gmail.com>
2018-02-11 17:43:17 -0200

    Update BubbleSort.swift

--- SwiftDataStructures.playground/Sources/BubbleSort/BubbleSort.swift
+++ SwiftDataStructures.playground/Sources/BubbleSort/BubbleSort.swift
@@ -2,7 +2,9 @@
  
  Bubble Sort Algorithm.
  
- The bubble sort algorithm loops though the array and compares if the array element n is bigger than n+1. When true, n and n+1 are swapped, and the loop continues until the biggest element in the array is moved to the last index. Then, the process is repeated until all elements in the array are put in ascending order.
+ The bubble sort algorithm loops though the array and compares if the array element n is bigger than n+1. When true, n and n+1
+ are swapped, and the loop continues until the biggest element in the array is moved to the last index. Then, the process is 
+ repeated until all elements in the array are put in ascending order.
  
  Eg: [4, 2, 7, 1, 3] -> [2, 4, 1, 7, 3] -> [2, 1, 4, 3, 7] -> [1, 2, 3, 4, 7]
  
